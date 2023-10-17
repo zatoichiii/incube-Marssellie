ced5d7685c182e68ccd6e9ed95911487423f474c
Слепов Данил Олегович <doslepov@maximum-avto.ru>
2023-10-17 19:32:52 +0300

    add componrnts + mobile

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Aiming from "./components/blocks/Aiming";
 import RulesTerms from "./components/blocks/RulesTerms";
 import Footer from "./components/blocks/Footer";
 import AwardsnCat from "./components/blocks/AwardsnCat";
+import Header from "./components/blocks/Header";
+import Banner from "./components/blocks/Banner";
+import Overview from "./components/blocks/Overview";
+import Operates from "./components/blocks/Operates";
+import CoreValues from "./components/blocks/CoreValues";
 
 function App() {
   return (
       <div className='App'>
+          <Header />
+          <Banner />
           <About/>
+          <Overview />
+          <CoreValues />
+          <Operates />
           <AwardsnCat/>
           <RulesTerms/>
           <Aiming/>
